6bb233319f243dd3ab76d35da07cbd444b000b01
Pascal <chiruclan@t-online.de>
2011-02-25 20:05:50 +0100

    config updated. can be edited manual now
    Signed-off-by: Pascal <chiruclan@t-online.de>

---
+++ lib/config.php
@@ -0,0 +1,10 @@
+<?php
+$sitename = "";
+$dbhost = "";
+$dbuser = "";
+$dbpasswd = "";
+$dbname = "";
+//do not touch following
+$version = "";
+$footer = "Copyright by ".$sitename." - wCMS v".$version;
+?>
