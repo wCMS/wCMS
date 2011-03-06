b86a0f802615bf38f1b2ce76185b31c4f3971ad6
Pascal <chiruclan@t-online.de>
2011-03-06 20:58:23 +0100

    Signed-off-by: Pascal <chiruclan@t-online.de>

--- install/upgrade.php
+++ install/upgrade.php
@@ -6,14 +6,14 @@ echo "Ist noch nicht installiert! <a href=\"index.php\">Klicke hier um es zu ins
 die;
 }
 include '../lib/config.php';
-elseif($version == $cmsversion) {
+if($version == $cmsversion) {
 echo "Kein Upgrade möglich.<br>Es wird bereits die aktuelle Version genutzt oder es wurden keine Veränderungen an der Datenbank vorgenommen";
 die;
 }
-elseif($version == "1.1") {
+if($version == "1.1") {
 header("Location: upgrade_1.1_1.2.php");
 }
-elseif($version == "1.2") {
+if($version == "1.2") {
 header("Location: upgrade_1.2_1.3.php");
 }
 ?>
