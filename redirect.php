5cceb0843f8214ca60ea9df4aff657e4bd98c5ca
Passi <chiruclan@t-online.de>
2011-03-29 23:58:01 +0200

    TRANSLATE/REDIRECT
    Signed-off-by: Passi <chiruclan@t-online.de>

--- redirect.php
+++ redirect.php
@@ -2,8 +2,8 @@
 include "lib/config.php";
 include "lib/header.php";
 ?>
-<title>Redirecting - <? echo $sitename ?></title>
-<center>You will be redirected in 3 seconds</center>
+<title>Weiterleiten - <? echo $sitename ?></title>
+<center>Du wirst in 3 Sekunden weitergeleitet</center>
 <head><meta http-equiv="refresh" content="3; <? echo $_SERVER['QUERY_STRING'] ?>"></head>
 <?
 echo "<hr>".$footer;
