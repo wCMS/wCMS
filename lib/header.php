ddac17375f58ce82b5ee9f898561bd354f88b257
Passi <chiruclan@t-online.de>
2011-03-31 17:40:51 +0200

    header fixxed

--- lib/header.php
+++ lib/header.php
@@ -11,12 +11,12 @@ $result = mysql_query($sql);
 $rows = mysql_num_rows($result);
 if($rows == 1) { 
 echo " | <a href=\"admin.php\">Adminpanel</a>";
+}
 if(!isset($_SESSION['all_user_id'])) {
 echo " | <a href=\"login.php\">Einloggen</a> oder <a href=\"login.php?mode=register\">Registrieren</a>";
 }
 else {
 echo " | <a href=\"login.php?mode=logout\">Ausloggen</a>";
-}
 }
 ?>
 <hr>
